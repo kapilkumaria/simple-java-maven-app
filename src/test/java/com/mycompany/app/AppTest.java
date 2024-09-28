8c2d2f0eff564796601e5ee447cc22547c6f1ccd
Kapil Kumaria <kapil.kumaria@gmail.com>
2024-09-28 02:54:29 -0600

    Modified App.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World of Devops!!!!", app.getMessage());
+        assertEquals("Hello World of DevOps!!!!", app.getMessage());
     }
 }
